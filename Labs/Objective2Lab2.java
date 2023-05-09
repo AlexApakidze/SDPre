5d332a8740359415713dd2f050056b8863dc978f
Alex A <Alexapakidze@yahoo.com>
2023-05-09 11:52:53 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Alex Apakidze";
+    int age = 31;
+    String state = "Maryland";
+
+    System.out.println ("Name:" + name + " Age:" + age + " State:" +state);
+    
+  }
+}
