b26007c76d03ad23b8b52310ca4df7843d53c73a
Alex A <Alexapakidze@yahoo.com>
2023-05-09 14:39:34 -0600

    Finish Objective2Lab3

--- Labs/Objective2Lab3.java
+++ Labs/Objective2Lab3.java
@@ -1,20 +1,14 @@
-import java.time.LocalDate;
-import java.time.DayOfWeek;
-
-
 public class Objective2Lab3 {
   public static void main(String[] args) {
 
-    LocalDate today = LocalDate.now();
-    DayOfWeek dayOfWeek = today.getDayOfWeek();
-    boolean isWeekend = (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY);
     int year = 1991;
     String month = "August";
+    boolean isWeekend = false;
 
     System.out.println(year);
     System.out.println(month);
     System.out.println(isWeekend);
-    
+
 
   }
 }
