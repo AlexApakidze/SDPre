cf2396f4712b49dda631cadba88dd5fdf326b50a
Alex A <Alexapakidze@yahoo.com>
2023-05-10 13:21:23 -0600

    complete Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,36 @@
+public class Objective7Lab4 {
+	public static void main (String[] args) {
+
+    int count =0; 
+    int sum =0;
+
+    do{
+      count= (count+1);
+	  sum= (count+sum);
+
+    } while(count <20);{
+
+
+		
+	     
+        
+         
+    }
+
+    System.out.println (sum);}    
+
+
+
+
+
+		
+
+
+
+
+
+
+
+
+	
+}
