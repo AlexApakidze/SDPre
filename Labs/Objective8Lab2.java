cc004b4ae721093029fd1c2ea6b34f85bc1da14a
Alex A <Alexapakidze@yahoo.com>
2023-05-10 15:00:05 -0600

    complete Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,27 @@
+public class Objective8Lab2 {
+	public static void main(String[] args){
+
+		for (int count=0, sum=0; count<21; count+=1){
+
+			sum = (count+sum);
+		
+
+
+		   if(count ==20){
+		    System.out.println(sum);}	
+		}
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+}
