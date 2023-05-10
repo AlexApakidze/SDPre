eb41f7d3c92cf8c15b61ffc4f873bcde83f46001
Alex A <Alexapakidze@yahoo.com>
2023-05-10 14:44:49 -0600

    complete Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,11 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+   
+   for(int i=1; i < 11; i+=1){
+    System.out.println(i);
+   }
+   for (int x=10; x > 0; x-=1){
+    System.out.println(x);
+   }
+  }
+}
